a56f2f67d780c5468af770f5302423089be63765
marwenDevBannour <marwen.bannour.dev@gmail.com>
2022-12-03 16:20:12 +0100

    Ajoutez des fichiers projet.

---
+++ DataAccess/Data/ProductData.cs
@@ -0,0 +1,69 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ProductData : IProductData
+    {
+        private readonly ISqlDataAccess _db;
+        public ProductData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+           return await _db.LoadData<Product, dynamic>(storeProcedure: "dbo.spProduct_GetAll", new { });
+            
+        }
+            
+        public async Task<Product> GetProduct(int id)
+        {
+            IEnumerable<Product> result = await _db.LoadData<Product, dynamic> (storeProcedure: "dbo.spProduct_Get", new { id});
+             return result.FirstOrDefault();
+        }
+
+        public Task InsertProduct(Product product)=>
+    
+            _db.SaveData(storedProcedure: "spProduct_Insert", new
+            {
+                product.Title,
+                product.Description,
+                product.Image,
+                product.IsPublic,
+                product.IsDelete,
+                product.CategoryId,
+                product.DateCreated,
+                product.DateUpdate,
+                product.Views
+            });
+
+        public Task UpdateProduct(Product product,int id) =>
+
+           _db.SaveData(storedProcedure: "spProduct_Update", new
+           {
+             id,
+               product.Title,
+               product.Description,
+               product.Image,
+               product.IsPublic,
+               product.IsDelete,
+               product.CategoryId,
+               product.DateCreated,
+               product.DateUpdate,
+               product.Views
+           });
+        public Task DeleteProduct(int id) =>
+
+        _db.SaveData(storedProcedure: "spProduct_Delete", new
+        {
+            Id=id
+        });
+    }
+}
---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccess.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "SqlConnection");
+        Task SaveData<T>(string storedProcedure, T parameters, string connctionId = "SqlConnection");
+    }
+}
