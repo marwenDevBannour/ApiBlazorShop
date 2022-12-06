dc4f82d5f973620129f856564f8384e0bd231913
marwenDevBannour <115179967+marwenDevBannour@users.noreply.github.com>
2022-12-06 20:27:59 +0000

    Deploying to gh-pages from  @ 8267d97a71de990d172d3f45426f38bfafb8bb64 🚀

--- DataAccess/DbAccess/ISqlDataAccess.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DataAccess.DbAccess
-{
-    public interface ISqlDataAccess
-    {
-        Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure, U parameters, string connectionId = "SqlConnection");
-        Task SaveData<T>(string storedProcedure, T parameters, string connctionId = "SqlConnection");
-    }
-}
